4bd088478e36ab5d1d4e6ccba1aaf47e2875966f
Artem <bezkorovainy2006@gmail.com>
2024-03-21 20:09:18 +0100

    customized document data

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
 import { AppProps } from "next/app";
 import "/styles/globals.css";
+import Head from "next/head";
+import React from "react";
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return <>
+    <Head>
+        <title>NEXT JS APP</title>
+        <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <Component {...pageProps} />
+  </>;
 }
