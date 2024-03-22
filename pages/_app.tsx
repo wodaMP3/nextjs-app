fd9cace9685366bf5cd2d5c179e3db912c48580c
Artem <bezkorovainy2006@gmail.com>
2024-03-22 19:04:02 +0100

    added Htag component, customized css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,6 +8,9 @@ export default function App({ Component, pageProps }: AppProps): JSX.Element {
     <Head>
         <title>NEXT JS APP</title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com"/>
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet" />
     </Head>
     <Component {...pageProps} />
   </>;
