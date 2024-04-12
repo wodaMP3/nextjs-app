89ba8d70245ed6bedb7a7a8a781f81932921e96d
Artem <bezkorovainy2006@gmail.com>
2024-04-12 20:04:02 +0200

    added almost completed Sort Component, unfixed problem w/ seo H1/2/3 in topPage

---
+++ components/Sort/Sort.props.ts
@@ -0,0 +1,13 @@
+import { DetailedHTMLProps, HTMLAttributes } from "react";
+
+export interface SortProps extends DetailedHTMLProps
+<HTMLAttributes<HTMLDivElement>, 
+HTMLDivElement >{
+    sort: SortEnum;
+    setSort: (sort: SortEnum) => void;
+}
+
+export enum SortEnum {
+    Rating, 
+    Price
+}
--- page-components/TopPageComponent/TopPageComponent.tsx
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -1,7 +1,8 @@
-import { Htag, Tag, HhData, Advantages, P } from '../../components';
+import { Htag, Tag, HhData, Advantages, Sort } from '../../components';
 import { TopPageComponentProps } from './TopPageComponent.props';
 import styles from './TopPageComponent.module.css';
 import { TopLevelCategory } from '../../interfaces/page.interface';
+import { SortEnum } from '../../components/Sort/Sort.props';
 
 export const TopPageComponent = ({ page, products, firstCategory }: TopPageComponentProps): JSX.Element => {
 	return (
@@ -9,7 +10,7 @@ export const TopPageComponent = ({ page, products, firstCategory }: TopPageCompo
 		<div className={styles.title}>
 			<Htag tag='h1'>{page.title}</Htag>
 			{products && <Tag color='grey' size='m'>{products.length}</Tag>}
-			<span>Sort Component</span>
+			<Sort sort={SortEnum.Rating} setSort={() => { }}/>
 		</div>
 		<div>
 			{products && products.map(p => (<div key={p._id}>{p.title}</div>))}
@@ -24,7 +25,7 @@ export const TopPageComponent = ({ page, products, firstCategory }: TopPageCompo
 				<Advantages advantages={page.advantages}/>
 			</>
 			}
-			{page.seoText && <P>{page.seoText}</P>}
+			{page.seoText && <div className={styles.seo} dangerouslySetInnerHTML={{ __html: page.seoText }} />}
 			<Htag tag='h2'>Skills u can get</Htag>
 			{page.tags.map(t => <Tag key={t} color='primary'>{t}</Tag>)}
 		</div>
