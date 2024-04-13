fd9015f32464b243b039f00d8efbce3580782e7d
Artem <bezkorovainy2006@gmail.com>
2024-04-13 19:34:29 +0200

    changed HtmlAttributes to InputHTMLAttributes in input ts, fixed placeholder problem

--- components/Input/Input.props.ts
+++ components/Input/Input.props.ts
@@ -1,7 +1,7 @@
-import { DetailedHTMLProps, HTMLAttributes } from "react";
+import { DetailedHTMLProps, InputHTMLAttributes } from "react";
 
 export interface InputProps extends DetailedHTMLProps
-<HTMLAttributes<HTMLInputElement>, 
+<InputHTMLAttributes<HTMLInputElement>, 
 HTMLInputElement> {
 
 }
