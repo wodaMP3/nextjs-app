75d2c24a7e71435f6b5a2528e949a6e1a22c9573
Artem <bezkorovainy2006@gmail.com>
2024-04-14 18:14:56 +0200

    added products net

--- components/Product/Product.tsx
+++ components/Product/Product.tsx
@@ -1,11 +1,23 @@
 import { ProductProps } from './Product.props';
 import styles from './Product.module.css';
 import cn from 'classnames';
+import { Card } from '../Card/Card';
+import { Rating } from '../Rating/Rating';
+import { Tag } from '../Tag/Tag';
 
 export const Product = ({ product, className, ...props }: ProductProps): JSX.Element => {
 	return (
-		<div >
-			{product.title} 
-		</div>
+		<Card >
+			<div className={styles.logo}><img src={product.image} alt={product.title} /></div>
+			<div className={styles.title}>{product.title}</div>
+			<div className={styles.price}>{product.price}</div>
+			<div className={styles.credit}>{product.credit}</div>
+			<div className={styles.rating}><Rating rating={product.reviewAvg ?? product.initialRating}/></div>
+			<div className={styles.tags}>{product.categories.map(c => 
+					<Tag key={c} color='ghost'>{c}</Tag>)} </div>
+			<div className={styles.priceTitle}>Price</div>
+			<div className={styles.creditTitle}>credit</div>
+			<div className={styles.rateTitle}>{product.reviewCount} reviews</div>
+		</Card>
 	);	
 };
