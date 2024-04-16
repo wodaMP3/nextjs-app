c863f4c12ae22e50812c6647c8026ae495ff0fcd
Artem <bezkorovainy2006@gmail.com>
2024-04-16 20:26:38 +0200

    updated css and reviewForm.tsx

--- components/ReviewForm/ReviewForm.tsx
+++ components/ReviewForm/ReviewForm.tsx
@@ -1,6 +1,7 @@
 import { ReviewFormProps } from './ReviewForm.props';
 import styles from './ReviewForm.module.css';
 import cn from 'classnames';
+import CloseIcon from './close.svg';
 import { Input } from '../Input/Input';
 import { Rating } from '../Rating/Rating';
 import { Textarea } from '../Textarea/Textarea';
@@ -8,20 +9,29 @@ import { Button } from '../button/Button';
 
 export const ReviewForm = ({ productId, className, ...props }: ReviewFormProps): JSX.Element => {
 	return (
-		<div className={cn(styles.reviewForm, className)}
-			{...props}
-		>
-			<Input />
-			<Input />
-			<div className={styles.rating}>
-				<span>Оценка:</span>
-				<Rating rating={0} />
+		<>
+			<div className={cn(styles.reviewForm, className)}
+				{...props}
+			>
+				<Input placeholder='Name'/>
+				<Input placeholder='Title of review' className={styles.title}/>
+				<div className={styles.rating}>
+					<span>Оценка:</span>
+					<Rating rating={0} />
+				</div>
+				<Textarea placeholder='Text of review' className={styles.description}/>
+				<div className={styles.submit}>
+					<Button appearance='primary'>Send</Button>
+					<span className={styles.info}>Before publishing your review will be checked</span>
+				</div>
+				<div className={styles.success}>
+					<div className={styles.successTitle}>Your review was sent!</div>
+					<div>
+						Thanks! Your review will be published after the inspection		
+					</div>
+					<CloseIcon className={styles.close}/>
+				</div>
 			</div>
-			<Textarea />
-			<div className={styles.submit}>
-				<Button appearance='primary'>Send</Button>
-				<span>Before publishing text will be sent to moderating check</span>
-			</div>
-		</div>
+		</>
 	);
 };
